e8368274edbdda8a9f46bc349aeefbd0ad172d72
Sonu Kumar <sonu82256@gmail.com>
2024-08-07 19:09:23 +0530

    resolved path error at the time of deployment

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
+import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
-import listingRouter from "./routes/listing.route.js";
+import listingRouter from "./routes/listing.routes.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 dotenv.config();
